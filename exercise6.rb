ce9bbeba4f92e27d892fa99a906967379c4d11de
Hira Kazmi <hirarukh@gmail.com>
2016-06-01 14:44:33 -0400

    checked for bananas, sorted alphabetically

--- exercise6.rb
+++ exercise6.rb
@@ -1,9 +1,19 @@
 grocery_list = [ "mangos", "spices", "potatoes", "chocolate"]
 grocery_list << "rice"
-
+grocery_list.sort!
+has_bananas = false
 
 grocery_list.each do |item|
   puts "* #{item}"
+  if item == "bananas"
+    has_bananas = true
+  end
+end
+
+if has_bananas
+  puts " You need to pick up bananas"
+else
+  puts " You dont need to pick up bananas"
 end
 
 puts grocery_list.length
